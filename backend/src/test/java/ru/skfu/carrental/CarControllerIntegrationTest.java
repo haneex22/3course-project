a80d9e0b87eada16a9e3dea0964fb3728a683774
haneex22 <Bekhan_dzh@mail.ru>
2026-06-21 17:18:45 +0300

    Fix: CI tests + gitignore gradle-wrapper.jar

--- backend/src/test/java/ru/skfu/carrental/CarControllerIntegrationTest.java
+++ backend/src/test/java/ru/skfu/carrental/CarControllerIntegrationTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.skfu.carrental.entity.User;
 import ru.skfu.carrental.entity.enums.UserRole;
@@ -20,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:integration-test.properties")
 class CarControllerIntegrationTest {
 
     @Autowired
